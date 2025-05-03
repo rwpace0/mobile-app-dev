86bbebd054d9a475b979c1bfc886b391f4f213bf
Reid Pace <rwpace06@gmail.com>
2025-05-02 20:59:45 -0700

    styling init

--- Workout/App.jsx
+++ Workout/App.jsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import { StyleSheet } from "react-native";
 import LoginPage from "./pages/login";
 import HomePage from "./pages/home";
 
@@ -19,11 +18,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ Workout/styles/login.styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+// example styles
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    width: "80%",
+  },
+  button: {
+    backgroundColor: "#007BFF",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+});
